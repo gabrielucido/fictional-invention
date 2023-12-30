fa9b4eaa858b6420024185f11a6aef679bb256ff
Lucas Muzza <muzzalucas@gmail.com>
2023-12-30 02:01:44 -0300

    Added the Script for the Eletrical Charm

---
+++ Assets/Scripts/MovingPlatformHorizontal.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public enum PlatformState
+{
+    PATROL,
+    IDLE
+}
+public enum PatrolType
+{
+    SEQUENTIAL,
+    RANDOM
+}
+public class MovingPlatformHorizontal : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public int currentWayPointIndex;
+    public float speed;
+    public Transform target;
+    public Vector2 boxSize;
+    public PlatformState platformState;
+    public PatrolType patrolType;
+    private Animator _animator;
+    private Rigidbody2D _rb;
+    void Start()
+    {
+        currentWayPointIndex = 0;
+        _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        platformState = PlatformState.IDLE;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            StartCoroutine(PlayAnimation());
+        }
+        if(platformState == PlatformState.PATROL)
+        {
+            if(Vector3.Distance(transform.position, waypoints[currentWayPointIndex].position) <= 1)
+            {
+                if(patrolType == PatrolType.SEQUENTIAL)
+                {
+                    currentWayPointIndex++;
+                    currentWayPointIndex %= waypoints.Length;
+                }
+                else if (patrolType == PatrolType.RANDOM)
+                {
+                    currentWayPointIndex = Random.Range(0, waypoints.Length);
+                }
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        if(platformState == PlatformState.PATROL)
+        {
+            Vector2 targetVelocity = Vector2.zero;
+             if(transform.position.x < waypoints[currentWayPointIndex].position.x)
+            {
+                targetVelocity = new Vector2(speed,0f);
+            }
+            else
+            {
+                targetVelocity = new Vector2(-speed,0f);    
+            }
+            _rb.velocity = targetVelocity;
+        }    
+    }
+
+    IEnumerator PlayAnimation()
+    {
+        _animator.SetBool("Charging",true);
+        yield return new WaitForSeconds(1);
+        // Start the patrol after the animation is done
+        StartMoving();
+
+    }
+
+    public void StartMoving()
+    {
+        platformState = PlatformState.PATROL;
+    }  
+}
---
+++ Assets/Scripts/MovingPlatformVertical.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public enum PlatformsState
+{
+    PATROL,
+    IDLE
+}
+
+public enum PatrolsType
+{
+    SEQUENTIAL,
+    RANDOM
+}
+
+public class MovingPlatformVertical : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public int currentWayPointIndex;
+    public float speed;
+    public Transform target;
+    public Vector2 boxSize;
+    public PlatformsState platformsState;
+    public PatrolsType patrolsType;
+    private Animator _animator;
+    private Rigidbody2D _rb;
+
+    void Start()
+    {
+        currentWayPointIndex = 0;
+        _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        platformsState = PlatformsState.IDLE;
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            StartCoroutine(PlayAnimation());
+        }
+        if(platformsState == PlatformsState.PATROL)
+        {
+            if(Vector3.Distance(transform.position, waypoints[currentWayPointIndex].position) <= 1)
+            {
+                if(patrolsType == PatrolsType.SEQUENTIAL)
+                {
+                    currentWayPointIndex++;
+                    currentWayPointIndex %= waypoints.Length;
+                }
+                else if (patrolsType == PatrolsType.RANDOM)
+                {
+                    currentWayPointIndex = Random.Range(0, waypoints.Length);
+                }
+            }
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if (platformsState == PlatformsState.PATROL)
+        {
+            Vector2 targetVelocity = Vector2.zero;
+            if (transform.position.y < waypoints[currentWayPointIndex].position.y)
+            {
+                targetVelocity = new Vector2(0f, speed);
+            }
+            else
+            {
+                targetVelocity = new Vector2(0f, -speed);
+            }
+            _rb.velocity = targetVelocity;
+        }
+    }
+
+     IEnumerator PlayAnimation()
+    {
+        _animator.SetBool("Charging",true);
+        yield return new WaitForSeconds(1);
+        // Start the patrol after the animation is done
+        StartMoving();
+
+    }
+
+    public void StartMoving()
+    {
+        platformsState = PlatformsState.PATROL;
+    }
+}
